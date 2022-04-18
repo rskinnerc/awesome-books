93c92f468521d319d7a368e30a4f0d90511d0f89
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-04-18 12:24:02 -0500

    Add books listing html

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
-let books = [
-  {
-    title: 'Book 1',
-    author: 'Author 1'
-  },
-  {
-    title: 'Book 2',
-    author: 'Author 2'
-  },
-  {
-    title: 'Book 3',
-    author: 'Author 3'
-  },
+const books = [
+
 ];
 
-let booksContainer = document.getElementById('books-cont');
+const booksContainer = document.getElementById('books-cont');
+
+const displayBooks = () => {
+  booksContainer.innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+    const item = document.createElement('li');
+    item.innerHTML = `<span class="title">${books[i].title}</span> by ${books[i].author} <br>`;
+    const btn = document.createElement('button');
+    const divider = document.createElement('hr');
+    btn.setAttribute('data-index', i);
+    btn.textContent = 'Remove';
+    item.append(btn);
+    item.append(divider);
+    booksContainer.append(item);
+  }
+};
 
 function addBook(title, author) {
-  books.push({title: title, author: author});
-  console.log(books);
+  books.push({ title, author });
+  displayBooks();
 }
 
-document.forms[0].onsubmit = function(event) {
+document.forms[0].onsubmit = (event) => {
   event.preventDefault();
-  let thisForm = event.target;
-  let author = thisForm[0].value;
-  let title = thisForm[1].value;
+  const thisForm = event.target;
+  const title = thisForm[0].value;
+  const author = thisForm[1].value;
   addBook(title, author);
-}
+};
